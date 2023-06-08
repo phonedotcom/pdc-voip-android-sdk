8c18ff96adbabd62044a9c5e653f73cf687e4fa5
Hardik Chauhan <hchauhan@phone.com>
2023-06-08 17:17:59 +0530

    added new call backs and notificatio channel to test creating notification only from sdk

--- sipservice/src/main/java/net/gotev/sipservice/SipService.java
+++ sipservice/src/main/java/net/gotev/sipservice/SipService.java
@@ -1143,7 +1143,7 @@ public class SipService extends BackgroundService implements SipServiceConstants
                 resultIntent,
                 PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
         );
-        String channelId = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) ? SipServiceConstants.GENERIC_PDC_VOIP_NOTIFICATION_CHANNEL : "";
+        String channelId = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) ? "SipServiceConstants.SERVICE_NOTIFICATION_CHANNEL_ID" : "";
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, channelId)
                 .setContentText(callName);
         mBuilder.setContentIntent(resultPendingIntent);
