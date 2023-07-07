1b994154b4abb3ea69dc2c759aef62926f79d42a
Pratik JD <pdarji@phone.com>
2023-07-07 16:50:19 +0530

    Implemented parsing of FCM data in our class  - Now client can provide data and library helps verify if it's of Phone.com or not.  - Based on the response, client has to send for further process.

---
+++ sipservice/src/main/java/net/gotev/sipservice/helper/PhoneComFirebaseMessageHelper.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2023 Phone.com®, All Rights Reserved.
+ */
+package net.gotev.sipservice.helper
+
+import android.content.Context
+import com.google.gson.Gson
+import net.gotev.sipservice.SipServiceCommand
+import net.gotev.sipservice.SipServiceConstants
+import net.gotev.sipservice.model.FCMResponse
+import org.json.JSONException
+import org.json.JSONObject
+
+
+object PhoneComFirebaseMessageHelper {
+
+    private const val APNS_VOIP = "APNS_VOIP"
+    private const val APNS_VOIP_SANDBOX = "APNS_VOIP_SANDBOX"
+
+    /**
+     * Validates provided firebase message data and return true if messages are related to Phone.com
+     * call notification. Validate message before process.
+     */
+    fun validate(messageData: String?): Boolean {
+        messageData?.let {
+            val json: JSONObject
+            var processedData: String? = null
+            try {
+                json = JSONObject(messageData)
+                if (json.has(APNS_VOIP)) {
+                   processedData  = json.getString(APNS_VOIP)
+                }
+                if (json.has(APNS_VOIP_SANDBOX)) {
+                    processedData = json.getString(APNS_VOIP_SANDBOX)
+                }
+
+                val fcmResponse = Gson().fromJson(processedData, FCMResponse::class.java)
+                return fcmResponse.notifyType == SipServiceConstants.PDC_CALL_NOTIFICATION
+
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+        }
+        return false
+    }
+
+
+    /**
+     * Process push message data and provides callback for the action Incoming or Missed call event
+     * Also refer @see BroadcastEventReceiver#onIncomingCall()
+     *
+     * @param context Application Context
+     * @param messageData Remote message data received as Push Notification from server
+     */
+    fun processMessageData(context: Context, messageData: String?) {
+        if (validate(messageData)) {
+            val data = Gson().fromJson(messageData, FCMResponse::class.java)
+
+            SipServiceCommand.handleIncomingCallPushNotification(
+                context,
+                data.status,
+                data.from,
+                data.server,
+                data.slot,
+                data.linkedUUID,
+                data.callerName
+            )
+
+        } else {
+            throw IllegalArgumentException("Invalid data received. Please refer document for valid push message format.")
+        }
+    }
+}
