12709fd5dcff9411ece86f32942769252b2d57f6
Hardik Chauhan <hchauhan@phone.com>
2023-07-13 04:20:56 +0530

    updated constants

--- sipservice/src/main/java/com/phone/sip/constants/PhoneComServiceConstants.kt
+++ sipservice/src/main/java/com/phone/sip/constants/PhoneComServiceConstants.kt
@@ -1,8 +1,8 @@
 package com.phone.sip.constants
 
 public object PhoneComServiceConstants {
-    const val PHONE_COM_SERVICE_NOTIFICATION_CHANNEL_NAME = "Phone Service Channel"
-    const val PHONE_COM_SERVICE_NOTIFICATION_CHANNEL_ID = "serviceNotificationChannelId"
-    const val PHONE_COM_MISSED_NOTIFICATION_ID = 1674
-    const val PHONE_COM_SERVICE_FOREGROUND_NOTIFICATION_ID = 121
+    const val SERVICE_NOTIFICATION_CHANNEL_NAME = "Phone Service Channel"
+    const val SERVICE_NOTIFICATION_CHANNEL_ID = "serviceNotificationChannelId"
+    const val MISSED_NOTIFICATION_ID = 1674
+    const val SERVICE_FOREGROUND_NOTIFICATION_ID = 121
 }
