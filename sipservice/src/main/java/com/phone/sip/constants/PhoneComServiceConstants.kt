869b92e5b87a396d38167df775045811d2bf505d
Hardik Chauhan <hchauhan@phone.com>
2023-07-13 04:16:19 +0530

    removed unnecessary constants and commented the sipAccount.setRegistration() method in addAccount() for the same issue faced in PDC app

--- sipservice/src/main/java/com/phone/sip/constants/PhoneComServiceConstants.kt
+++ sipservice/src/main/java/com/phone/sip/constants/PhoneComServiceConstants.kt
@@ -1,14 +1,8 @@
 package com.phone.sip.constants
 
 public object PhoneComServiceConstants {
-    const val GENERIC_PDC_VOIP_NOTIFICATION_CHANNEL = "GENERIC_PDC_VOIP_NOTIFICATION_CHANNEL"
     const val SERVICE_NOTIFICATION_CHANNEL_NAME = "Phone Service Channel"
     const val SERVICE_NOTIFICATION_CHANNEL_ID = "serviceNotificationChannelId"
-    const val MISS_CALL_NOTIFICATION_CHANNEL = "missCallNotificationChannelId"
-    const val INCOMING_CALL_NOTIFICATION_CHANNEL_ID = "incomingCallNotificationChannelId"
     const val MISSED_NOTIFICATION_ID = 1674
     const val SERVICE_FOREGROUND_NOTIFICATION_ID = 121
-
-    const val HANGUP_BROADCAST_ACTION_ID = 2
-    const val ACCEPT_CALL_BROADCAST_ACTION_ID = 1
 }
