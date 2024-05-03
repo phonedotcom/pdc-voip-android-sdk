d0c9e17957f838bdf84b35170906421c5176f7ba
Hardik Chauhan <hchauhan@phone.com>
2024-05-03 17:42:47 +0530

    Broadcast FALG change for android 14 support

--- sipservice/src/main/java/com/phone/sip/BroadcastEventReceiver.java
+++ sipservice/src/main/java/com/phone/sip/BroadcastEventReceiver.java
@@ -6,8 +6,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 
 import com.phone.sip.constants.CallEvent;
 import com.phone.sip.constants.CallMediaEvent;
@@ -168,7 +170,16 @@ public class BroadcastEventReceiver extends BroadcastReceiver implements SipServ
                 BroadcastEventEmitter.BroadcastAction.HOLD_CALL));
         intentFilter.addAction(BroadcastEventEmitter.getAction(
                 BroadcastEventEmitter.BroadcastAction.RESUME_CALL));
-        context.registerReceiver(this, intentFilter);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ContextCompat.registerReceiver(
+                    context,
+                    this,
+                    intentFilter,
+                    ContextCompat.RECEIVER_NOT_EXPORTED
+            );
+        } else {
+            context.registerReceiver(this, intentFilter);
+        }
     }
 
     /**
